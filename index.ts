1ac2d01a47e852bbb951647350cc7ac0bd70684a
kwongwangjae <kgy30002@gmail.com>
2023-07-02 17:47:52 +0900

    feat: front setting

--- index.ts
+++ index.ts
@@ -7,6 +7,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Typescript + Node.js + Express Server");
 });
 
+app.get("/path1", (req: Request, res: Response) => {
+  res.json({ id: "권광재", name: "김치" });
+});
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at <https://localhost>:${port}`);
 });
